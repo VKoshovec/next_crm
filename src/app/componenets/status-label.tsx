c05210648fb7e6ed24febf07197cfa390fc50dc8
VladimirKoshovec <koshavladmir@gmail.com>
2024-06-26 10:03:27 +0300

    clsx

--- src/app/componenets/status-label.tsx
+++ src/app/componenets/status-label.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import clsx from "clsx";
 
 export enum Status {
     active="active",
@@ -10,15 +11,22 @@ export enum Status {
 export interface StatusLabelProps {
     children: React.ReactNode;
     status: Status;
+    disabled?: Boolean;
 };
 
-export default function StatusLabel({ children, status }:StatusLabelProps){
-    return <div className={`inline-flex items-center py-1 px-3.5 rounded-3xl text-sm font-medium ${
-        status === Status.active && 'text-green-700 bg-green-100' ||
-        status === Status.notactive && 'text-red-700 bg-red-100' ||
-        status === Status.pending && 'text-orange-700 bg-orange-100' ||
-        status === Status.suspended && 'text-blue-700 bg-blue-100' || ""
-    }`}>
+export default function StatusLabel({ children, status, disabled }:StatusLabelProps){
+    return <div className={clsx(
+           `inline-flex items-center py-1 px-3.5 rounded-3xl text-sm font-medium`, 
+            status === Status.active && 'text-green-700 bg-green-100',
+            status === Status.notactive && 'text-red-700 bg-red-100',
+            status === Status.pending && 'text-orange-700 bg-orange-100',
+            status === Status.suspended && 'text-blue-700 bg-blue-100', 
+            disabled && 'opacity-75 cursor-not-allowed'
+            // xz vas is das -->
+            // {
+            //     ['opacity-75 cursor-not-allowed']:disabled
+            // }
+        )}>
     <div className="w-1 h-1 mr-2 rounded-full bg-current"></div>    
     { children }
     </div>
