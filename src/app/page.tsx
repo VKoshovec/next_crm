e7aa15279b2eae545d64a2c196c83281e9047651
VladimirKoshovec <koshavladmir@gmail.com>
2024-06-28 10:11:33 +0300

    add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,19 @@
+import AddCompanyButton from "./componenets/add-company-button";
 import StatusLabel, { Status } from "./componenets/status-label";
 
 
 export default function Home() {
   return (
     <main>
-      <h1>HomePage</h1>
+      <h1 className="italic font-bold">HomePage</h1>
       <StatusLabel status={Status.active}>Active</StatusLabel>
       <StatusLabel status={Status.notactive}>Not Active</StatusLabel>
       <StatusLabel status={Status.pending}>Pending</StatusLabel>
       <StatusLabel status={Status.suspended}>Suspended</StatusLabel>
+      <div className="p-10">
+        <AddCompanyButton/>
+      </div>
+      
     </main>
   );
 }
