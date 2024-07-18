1fb3fb4a3c7f7fa4682676768e615bf783b4377e
VladimirKoshovec <koshavladmir@gmail.com>
2024-07-18 10:35:39 +0300

    add_some

--- src/app/componenets/sidebar.tsx
+++ src/app/componenets/sidebar.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import Image from 'next/image';
 import SidebarItem from './sidebar-item';
 import { usePathname, useRouter } from 'next/navigation';
+import Link from 'next/link';
 
 
 
@@ -22,13 +23,15 @@ export default function Sidebar({}: SidebarProps) {
   return (
     <aside className="fixed top-0 left-0 z-40 w-60 h-screen">
       <div className="flex flex-col h-full overflow-y-auto bg-gray-900">
-        <Image
-          className="py-8 mb-11 mx-auto"
-          width={122}
-          height={25}
-          src="/icons/logo.svg"
-          alt="logo"
-        />
+        <Link href={'/'}>
+           <Image
+              className="py-8 mb-11 mx-auto"
+              width={122}
+              height={25}
+              src="/icons/logo.svg"
+              alt="logo"
+            />
+        </Link>
         <ul className="space-y-7">
           <SidebarItem
             current = { pathname === "/dashboard" }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,37 @@
-'use client'
-
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import Sidebar from "./componenets/sidebar";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
 
-  const router = useRouter();
-
-  useEffect(()=>{
-    router.push('/dashboard');
-  },[router])
-
-  return; 
-
-  // return (
-  //   <main>
-  //     <h1 className="text-xl">Home page</h1>
-  //   </main>
-  // );
+  return (
+    <main>
+      <Sidebar/>
+      <h1 className="text-xl ml-64 mt-8">Home page</h1>
+      <ul className="flex justify-center flex-col ml-64 mt-10">
+        <li>
+          <Link href={'/dashboard'}>      
+             <Image 
+                alt="dashboard"
+                src={'/images/dashboard.png'}
+                width='500'
+                height='200'
+                className="mb-10 hover:scale-105"
+              />
+          </Link>
+          </li>
+        <li>
+          <Link href={'/companies'}>  
+            <Image 
+                alt="companies"
+                src={'/images/companies.png'}
+                width='500'
+                height='200'
+                className="hover:scale-105"
+             />
+             </Link>
+        </li>
+      </ul>
+    </main>
+  );
 }
